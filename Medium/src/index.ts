50494545935ebedcd36ad919704c81a82e25d471
sourabhrajwade <rajwadesourabh@gmail.com>
2020-05-20 19:52:55 +0530

    page 1 and 2 design complete

---
+++ Medium/src/index.ts
@@ -0,0 +1,44 @@
+
+// interface userInfo {
+//     username: string;
+//     key?: string;
+// }
+// class LoginDetail {
+//     username: string;
+//     key?: string;
+//     constructor (userInputInfo:userInfo) {
+//         this.username = userInputInfo.username;
+//         this.key = userInputInfo.key;
+//     }
+// }
+
+// class User {
+//     url: string;
+//     image: string;
+
+// }
+// class Items {
+//     title: string;
+//     pubDate: string;
+//     link: string;
+//     author: string;
+//     description: string;
+//     content: string;
+//     categories: string[];
+
+// }
+
+class MediumApiService {
+    constructor (){
+       
+    }
+    async getUserAPI (user: string) {
+        const fetchAPI = await fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@" + userInputInfo.username);
+        const { data } = await fetchAPI.json();
+        console.log(data);
+    }
+
+}
+
+const myData = new MediumApiService();
+myData.getUserAPI("sourabhrajwade");
---
+++ Medium/src/promises.ts
@@ -0,0 +1,33 @@
+// const baseApi = 'https://reqres.in/api/users?page=1'
+// const userApi = 'https://reqres.in/api/user'
+
+// const fetchAllEmployees = async (url: string): Promise<Employee[]> => {
+//     const response = await fetch(url)
+//     const { data } = await response.json()
+//     return data
+// }
+
+// const fetchEmployee = async (url: string, id: number): Promise<Record<string, string>> => {
+//     const response = await fetch(`${url}/${id}`)
+//     const { data } = await response.json()
+//     return data
+// }
+// const generateEmail = (name: string): string => {
+//     return `${name.split(' ').join('.')}@company.com`
+// }
+
+// const runAsyncFunctions = async () => {
+//     try {
+//         const employees = await fetchAllEmployees(baseApi)
+//         Promise.all(
+//             employees.map(async user => {
+//                 const userName = await fetchEmployee(userApi, user.id)
+//                 const emails = generateEmail(userName.name)
+//                 return emails
+//             })
+//         )
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// runAsyncFunctions()
