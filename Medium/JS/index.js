2ea72a106530ffc344858e2dedb4602c2c005e6e
sourabhrajwade <rajwadesourabh@gmail.com>
2020-05-22 21:36:01 +0530

    card populated

--- Medium/JS/index.js
+++ Medium/JS/index.js
@@ -1,10 +1,10 @@
 window.addEventListener("DOMContentLoaded", function () {
    
-        const userName = document.getElementById('exampleInputEmail1').value;
-        console.log(userName);
+        const user = document.getElementById('exampleInputEmail1');
+        //console.log(user);
         const key = document.getElementById('exampleInputPassword1').value;
         const searchBar = document.getElementById('search');
-        console.log(key);
+        //console.log(key);
         const rowCard = document.getElementById("row-card");
         
         const app = {
@@ -45,6 +45,62 @@ window.addEventListener("DOMContentLoaded", function () {
             }
         }
         app.init();
+        const submit = document.querySelector('.btn-primary');
+        submit.addEventListener('click', getData);
+        async function getData () {
+            const userName = user.value;
+            const response = await fetch("https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@" + userName);
+            const data = await response.json();
+            //return data;
+            const container = document.querySelector('#content');
+            //const row = document.querySelector('#row-cards');
+            
+            
+               for (let i =0; i < data['items'].length; i++) {
+                    const row = document.querySelector('#row-cards');
+                    //let i =0;
+                    let j = i+1;
+                    let k = i+2;
+                    row.innerHTML = `   <div class="card-deck" justify-content-center align-items-center>
+                                        <div class="card" id="1" style="width: 18rem;" >
+                                            <img class="card-img-top" src="${data['items'][i].thumbnail}" alt="Card image cap">
+                                            <div class="card-body">
+                                                <h5 class="card-title">${data['items'][i].title}</h5>
+                                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                                <p class="card-text"><small class="text-muted">${data["items"][i].pubDate}</small></p>
+                                                <a href="${data["items"][i].guid}">Continue Reading</a>
+                                            </div>
+                                        </div>
+                                        <div class="card" id="2" style="width: 18rem;">
+                                            <img class="card-img-top" src="${data['items'][j].thumbnail}" alt="Card image cap">
+                                             <div class="card-body">
+                                                <h5 class="card-title">${data['items'][j].title}</h5>
+                                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                                <p class="card-text"><small class="text-muted">${data["items"][j].pubDate}</small></p>
+                                                <a href="${data["items"][j].guid}">Continue Reading</a>
+                                             </div>
+                                        </div>
+                                        <div class="card id="3" style="width: 18rem;">
+                                            <img class="card-img-top" src="${data['items'][k].thumbnail}" alt="Card image cap">
+                                            <div class="card-body">
+                                                <h5 class="card-title">${data['items'][k].title}</h5>
+                                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                                <p class="card-text"><small class="text-muted">${data["items"][k].pubDate}</small></p>
+                                                <a href="${data["items"][k].guid}">Continue Reading</a>
+                                            </div>
+                                        </div>
+                                    </div>`
+                    container.appendChild(row);
+                   i = i + 3 
+                }
+                
+            //console.log(data);
+
+        };
+        //getData('drew.costley')
+        
+        
+        
        
         
         
